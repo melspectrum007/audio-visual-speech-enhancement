71a7bcccb5373fc4b266f1532d0297f33c2e7130
avivga <avivga@gmail.com>
2017-10-16 18:39:39 +0300

    cluster cli: change default no. of gpus and cpus

--- cluster.py
+++ cluster.py
@@ -56,8 +56,8 @@ def main():
 	train_parser.add_argument('-mn', '--model', type=str, required=True)
 	train_parser.add_argument('-dn', '--data_name', type=str, required=True)
 	train_parser.add_argument('-m', '--mem', type=int, default=100000)
-	train_parser.add_argument('-g', '--gpus', type=int, default=2)
-	train_parser.add_argument('-c', '--cpus', type=int, default=8)
+	train_parser.add_argument('-g', '--gpus', type=int, default=1)
+	train_parser.add_argument('-c', '--cpus', type=int, default=4)
 	train_parser.add_argument('-t', '--time', type=str, default='30:0:0')
 	train_parser.set_defaults(func=train)
 
@@ -67,8 +67,8 @@ def main():
 	predict_parser.add_argument('-s', '--speakers', nargs='+', type=str, required=True)
 	predict_parser.add_argument('-n', '--noise_dirs', nargs='+', type=str, required=True)
 	predict_parser.add_argument('-m', '--mem', type=int, default=50000)
-	predict_parser.add_argument('-g', '--gpus', type=int, default=2)
-	predict_parser.add_argument('-c', '--cpus', type=int, default=8)
+	predict_parser.add_argument('-g', '--gpus', type=int, default=1)
+	predict_parser.add_argument('-c', '--cpus', type=int, default=4)
 	predict_parser.add_argument('-t', '--time', type=str, default='10:0:0')
 	predict_parser.set_defaults(func=predict)
 
