aaa12ffe759818e0a4dc226f9fc45cf635b0c315
asaph <asaph.shamir@gmail.com>
2017-10-23 17:01:58 +0300

    network: changed <= to > in early stopping

--- network.py
+++ network.py
@@ -372,7 +372,7 @@ class SpeechEnhancementNetwork(object):
             return False
         head = loss[:-patience]
         tail = loss[-patience:]
-        return tail.min() <= (head.min() - delta)
+        return tail.min() > (head.min() - delta)
 
     @staticmethod
     def __split_train_validation_data(arrays, validation_split):
