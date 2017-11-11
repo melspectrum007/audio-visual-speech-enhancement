4c279047b90a0e12d96483b909be491c2dd282b5
asaph <asaph.shamir@gmail.com>
2017-11-11 12:18:45 +0200

    source evaluate: added temp dir to script

--- source_enhancement_evaluator.py
+++ source_enhancement_evaluator.py
@@ -3,28 +3,33 @@ import os
 import subprocess
 import tempfile
 import re
-import uuid
-
+from shutil import copy2
 import numpy as np
 
 from mediaio import ffmpeg
 
 
 def pesq(pesq_bin_path, source_file_path, enhanced_file_path):
-	# temp_dir = tempfile.gettempdir()
-	#
-	# temp_source_path = os.path.join(temp_dir, str(uuid.uuid4()) + ".wav")
-	# temp_estimated_path = os.path.join(temp_dir, str(uuid.uuid4()) + ".wav")
-	#
+	temp_dir = tempfile.gettempdir()
+
+	temp_source_path = os.path.join(temp_dir, 'source.wav')
+	temp_enhanced_path = os.path.join(temp_dir, 'enhanced.wav')
+
+	copy2(source_file_path, temp_source_path)
+	copy2(enhanced_file_path, temp_enhanced_path)
+
 	# ffmpeg.downsample(source_file_path, temp_source_path, sample_rate=16000)
 	# ffmpeg.downsample(enhanced_file_path, temp_estimated_path, sample_rate=16000)
 
 	try:
 		output = subprocess.check_output(
-			[pesq_bin_path, "+16000", source_file_path, enhanced_file_path]
+			[pesq_bin_path, "+16000", temp_source_path, temp_enhanced_path]
 		)
 	except Exception:
+		print 'exception'
 		return None, None
+
+	# print output
 
 	match = re.search("\(Raw MOS, MOS-LQO\):\s+= (-?[0-9.]+?)\s+([0-9.]+?)$", output, re.MULTILINE)
 	mos = float(match.group(1))
@@ -40,13 +45,12 @@ def evaluate(enhancement_dir_path, pesq_bin_path):
 	enhanced_pesqs = []
 	mixture_pesqs = []
 
-
 	speaker_dir_names = os.listdir(enhancement_dir_path)
 	for speaker_dir_name in speaker_dir_names:
 		speaker_dir_path = os.path.join(enhancement_dir_path, speaker_dir_name)
-		sample_dir_names = os.listdir(speaker_dir_path)
+		sample_dir_names = sorted(os.listdir(speaker_dir_path))
 		for sample_dir_name in sample_dir_names:
-			# print sample_dir_name
+			print sample_dir_name
 			sample_dir_path = os.path.join(speaker_dir_path, sample_dir_name)
 			source_file_path = os.path.join(sample_dir_path, "source.wav")
 			enhanced_file_path = os.path.join(sample_dir_path, "enhanced.wav")
@@ -55,7 +59,7 @@ def evaluate(enhancement_dir_path, pesq_bin_path):
 			enhanced_mos, _ = pesq(pesq_bin_path, source_file_path, enhanced_file_path)
 			mixture_mos, _ = pesq(pesq_bin_path, source_file_path, mixture_file_path)
 
-			print 'mixture pesq: ', mixture_mos, 'enhanced pesq: ', enhanced_mos, 'mixture pesq lqo: '
+			print 'mixture pesq: ', mixture_mos, 'enhanced pesq: ', enhanced_mos
 			if enhanced_mos is None or mixture_mos is None:
 				continue
 			else:
