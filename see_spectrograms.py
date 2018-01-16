28b9d962b05028b5603a68a7ef1491335073b72c
asaph <asaph.shamir@gmail.com>
2018-01-16 11:16:41 +0200

    updated see specs

--- see_spectrograms.py
+++ see_spectrograms.py
@@ -6,30 +6,35 @@ import sys
 import os
 
 
-def plot_spec(path, show=True):
+def plot_spec(path, absolute, show=True):
 	spec = np.load(path)
-	if spec.min() >= 0:
-		spec = lb.amplitude_to_db(spec)
+	# if spec.min() >= 0:
+	# 	spec = lb.amplitude_to_db(spec)
+	if absolute:
+		spec = np.abs(spec)
+
 	plt.figure()
 	plt.pcolormesh(spec)
+	plt.colorbar()
 	plt.title(path.split('/')[-1][:-4])
 	if show:
 		plt.show()
 
-def plot_specs(path):
+def plot_specs(path, absolute):
 	for f in os.listdir(path):
 		if f.endswith('npy'):
 			file_path = os.path.join(path, f)
-			plot_spec(file_path, show=False)
+			plot_spec(file_path, absolute, show=False)
 
 	plt.show()
 
 def main():
 	path = sys.argv[1]
+	absolute = sys.argv[2]
 	if os.path.isdir(path):
-		plot_specs(path)
+		plot_specs(path, absolute)
 	else:
-		plot_spec(path)
+		plot_spec(path, absolute)
 
 if __name__ == "__main__":
 	main()
