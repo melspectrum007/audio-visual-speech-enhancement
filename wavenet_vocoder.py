8e3aeccd30d675d708b271ac9512b49a4494b94e
asaph <asaph.shamir@gmail.com>
2018-02-01 13:44:24 +0200

    finished vocoder architecture. Vocoder can process variable lenght spectrograms

--- wavenet_vocoder.py
+++ wavenet_vocoder.py
@@ -32,13 +32,13 @@ class WavenetVocoder(object):
 
 	def build_upsample_net(self, input_shape):
 		layer_input = Input(input_shape)
-		extended = Lambda(lambda a: tf.expand_dims(layer_input, axis=1))(layer_input)
+		extended = Lambda(lambda a: tf.expand_dims(a, axis=1))(layer_input)
 
 		x = Deconv2D(self.num_upsample_channels, kernel_size=(1, 4), strides=(1, 4), padding='same')(extended)
 		x = Deconv2D(self.num_upsample_channels, kernel_size=(1, 4), strides=(1, 4), padding='same')(x)
 		x = Deconv2D(self.num_upsample_channels, kernel_size=(1, 10), strides=(1, 10), padding='same')(x)
-		# x = Lambda(lambda a: tf.squeeze(x, axis=1))(x)
-		x = Reshape((3200,80))(x)
+		x = Lambda(lambda a: tf.squeeze(a, axis=1))(x)
+		# x = Reshape((3200,80))(x)
 
 		model = Model(layer_input, x, name='Upsample_Net')
 		model.summary()
@@ -55,11 +55,10 @@ class WavenetVocoder(object):
 
 		skips = []
 		for i in range(self.num_dilated_blocks):
-			out, skip = self.build_dilated_conv_block((3200, 16), 5, 2**(i % 10), 16, 16, number=i+1)(out)
+			out, skip = self.build_dilated_conv_block((None, 16), 5, 2**(i % 10), 16, 16, number=i+1)(out)
 			skips.append(skip)
 
-		stack = Lambda(lambda x: tf.stack(x, -1))(skips)
-		stack = Reshape((3200, 16 * self.num_dilated_blocks))(stack)
+		stack = Concatenate()(skips)
 		stack = Activation('relu')(stack)
 
 		MoL_params = Conv1D(30, 1)(stack)
@@ -89,6 +88,6 @@ def loss(y_true, y_pred):
 if __name__ == '__main__':
 	net = WavenetVocoder(80, 3)
 	# net.build_upsample_net((20, 80)).summary()
-	net.build((20, 80))
+	net.build((None, 80))
 
 
