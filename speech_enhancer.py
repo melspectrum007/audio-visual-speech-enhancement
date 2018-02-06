overfit bad, need to fix abs in loss, no normalization in loss, zero padding in label input

speech_enhancer.py
@@ -6,6 +6,7 @@ import data_processor as dp
 
 from dataset import AudioVisualDataset, AudioDataset
 from network import SpeechEnhancementNetwork
+from wavenet_vocoder import WavenetVocoder
 from shutil import copy2
 from mediaio import ffmpeg
 from datetime import datetime
@@ -41,7 +42,6 @@ def preprocess(args):
 		mixed_spectrograms=mixed_spectrograms,
 		source_spectrograms=source_spectrograms,
 		source_phases=source_phases,
-
 	)
 
 
@@ -269,6 +269,63 @@ def generate_vocoder_dataset(args):
 	)
 
 
+def train_vocoder(args):
+	cache_dir = os.path.join(args.base_folder, 'cache')
+	if not os.path.exists(cache_dir):
+		os.mkdir(cache_dir)
+	models_dir = os.path.join(cache_dir, 'models')
+	if not os.path.exists(models_dir):
+		os.mkdir(models_dir)
+	model_cache_dir = os.path.join(models_dir, args.model)
+	if not os.path.exists(model_cache_dir):
+		os.mkdir(model_cache_dir)
+
+	train_preprocessed_blob_path = os.path.join(args.base_folder, 'cache/preprocessed', args.train_data_name + '.npz')
+	val_preprocessed_blob_path = os.path.join(args.base_folder, 'cache/preprocessed', args.val_data_name + '.npz')
+
+	with np.load(train_preprocessed_blob_path) as data:
+		OVERFIT_NUM = 15 # todo: remove
+		train_enhanced_spectrograms = data['enhanced_spectrograms'][:OVERFIT_NUM]
+		train_waveforms = data['source_waveforms'][:OVERFIT_NUM]
+
+	with np.load(val_preprocessed_blob_path) as data:
+		val_enhanced_spectrograms = data['enhanced_spectrograms']
+		val_waveforms = data['source_waveforms']
+
+	val_enhanced_spectrograms = train_enhanced_spectrograms
+	val_waveforms = train_waveforms
+
+	print 'building network...'
+	network = WavenetVocoder(80, 15, (train_enhanced_spectrograms.shape[1:]))
+	network.train(train_enhanced_spectrograms, train_waveforms, val_enhanced_spectrograms, val_waveforms, model_cache_dir)
+
+
+def predict_vocoder(args):
+	model_cache_dir = os.path.join(args.base_folder, 'cache/models', args.model)
+	prediction_output_dir = os.path.join(args.base_folder, 'out', args.model)
+	testset_path = os.path.join(args.base_folder, 'cache/preprocessed', args.data_name + '.npz')
+	if not os.path.exists(prediction_output_dir):
+		os.mkdir(prediction_output_dir)
+
+	storage = PredictionStorage(prediction_output_dir)
+	network = WavenetVocoder.load(model_cache_dir)
+
+	with np.load(testset_path) as data:
+		OVERFIT_NUM = 15 # todo: remove
+		enhanced_spectrogarms = data['enhanced_spectrograms'][:OVERFIT_NUM]
+		source_waveforms = data['source_waveforms'][:OVERFIT_NUM]
+
+	for i in range(enhanced_spectrogarms.shape[0]):
+		wave_data = network.predict_one_sample(enhanced_spectrogarms[i][np.newaxis, ...])
+		enhanced_signal = AudioSignal(wave_data, 16000)
+		source_signal = AudioSignal(source_waveforms[i], 16000)
+
+		enhanced_signal.set_sample_type('int16')
+		source_signal.set_sample_type('int16')
+
+		storage.save_vocoder_pred(enhanced_signal, source_signal, i)
+
+
 class PredictionStorage(object):
 
 	def __init__(self, storage_dir):
@@ -313,6 +370,13 @@ class PredictionStorage(object):
 		# os.unlink(enhanced_speech_audio_path)
 
 		return sample_prediction_dir
+
+	def save_vocoder_pred(self, enhanced_signal, source_signal, num):
+		enhanced_path = os.path.join(self.__base_dir, 'enhanced_' + str(num) + '.wav')
+		source_path = os.path.join(self.__base_dir, 'source_' + str(num) + '.wav')
+		enhanced_signal.save_to_wav_file(enhanced_path)
+		source_signal.save_to_wav_file(source_path)
+
 
 	def save_spectrograms(self, spectrograms, names, dir_path):
 		for i, spec in enumerate(spectrograms):
@@ -373,6 +437,12 @@ def main():
 	train_parser.add_argument('-vdn', '--val_data_names', nargs='+', type=str, required=True)
 	train_parser.set_defaults(func=train)
 
+	train_vocoder_parser = action_parsers.add_parser('train_vocoder')
+	train_vocoder_parser.add_argument('-mn', '--model', type=str, required=True)
+	train_vocoder_parser.add_argument('-tdn', '--train_data_name', type=str, required=True)
+	train_vocoder_parser.add_argument('-vdn', '--val_data_name', type=str, required=True)
+	train_vocoder_parser.set_defaults(func=train_vocoder)
+
 	predict_parser = action_parsers.add_parser('predict')
 	predict_parser.add_argument('-mn', '--model', type=str, required=True)
 	predict_parser.add_argument('-ds', '--dataset', type=str, required=True)
@@ -380,6 +450,11 @@ def main():
 	predict_parser.add_argument('-is', '--ignored_speakers', nargs='+', type=str)
 	predict_parser.add_argument('-n', '--noise_dirs', nargs='+', type=str, required=True)
 	predict_parser.set_defaults(func=predict)
+
+	predict_vocoder_parser = action_parsers.add_parser('predict_vocoder')
+	predict_vocoder_parser.add_argument('-mn', '--model', type=str, required=True)
+	predict_vocoder_parser.add_argument('-dn', '--data_name', type=str, required=True)
+	predict_vocoder_parser.set_defaults(func=predict_vocoder)
 
 	test_parser = action_parsers.add_parser('test')
 	test_parser.add_argument('-mn', '--model', type=str, required=True)

wavenet_vocoder.py
@@ -3,16 +3,21 @@ import keras.backend as tf
 
 from keras.layers import *
 from keras.models import *
-
+from network import ModelCache
+from keras.callbacks import ModelCheckpoint, ReduceLROnPlateau, EarlyStopping
 
 
 class WavenetVocoder(object):
 
-	def __init__(self, num_upsample_channels, num_dilated_blocks):
-		self.num_upsample_channels = num_upsample_channels
-		self.num_dilated_blocks = num_dilated_blocks
+	def __init__(self, num_upsample_channels=None, num_dilated_blocks=None, spec_shape=None):
+		if num_upsample_channels is not None:
+			self.num_upsample_channels = num_upsample_channels
+			self.num_dilated_blocks = num_dilated_blocks
+			self.__model = self.build(spec_shape)
 
-	def build_dilated_conv_block(self, input_shape, kernel_size, dilation, num_dilated_filters, num_skip_filters, number=None):
+
+	@staticmethod
+	def build_dilated_conv_block(input_shape, kernel_size, dilation, num_dilated_filters, num_skip_filters, number=None):
 		layer_input = Input(input_shape)
 
 		filters = Conv1D(num_dilated_filters, kernel_size, padding='same', dilation_rate=dilation, activation='tanh')(layer_input)
@@ -28,17 +33,19 @@ class WavenetVocoder(object):
 		else:
 			model = Model(layer_input, outputs=[output, skip])
 
+		model.summary()
+
 		return model
 
 	def build_upsample_net(self, input_shape):
 		layer_input = Input(input_shape)
-		extended = Lambda(lambda a: tf.expand_dims(a, axis=1))(layer_input)
+		permuted = Permute((2, 1))(layer_input)
+		extended = Lambda(lambda a: tf.expand_dims(a, axis=1))(permuted)
 
 		x = Deconv2D(self.num_upsample_channels, kernel_size=(1, 4), strides=(1, 4), padding='same')(extended)
 		x = Deconv2D(self.num_upsample_channels, kernel_size=(1, 4), strides=(1, 4), padding='same')(x)
 		x = Deconv2D(self.num_upsample_channels, kernel_size=(1, 10), strides=(1, 10), padding='same')(x)
 		x = Lambda(lambda a: tf.squeeze(a, axis=1))(x)
-		# x = Reshape((3200,80))(x)
 
 		model = Model(layer_input, x, name='Upsample_Net')
 		model.summary()
@@ -64,30 +71,87 @@ class WavenetVocoder(object):
 		MoL_params = Conv1D(30, 1)(stack)
 
 		model = Model(spectrogram, outputs=[MoL_params], name='Vocoder')
+		optimizer = optimizers.adam(lr=5e-4)
+		model.compile(loss=loss, optimizer=optimizer)
+
 		model.summary()
 
 		return model
 
+	def train(self, train_enhanced_spectrograms, train_waveforms, val_enhanced_spectrograms, val_waveforms, model_cache_dir):
+		train_waveforms = np.c_[train_waveforms, np.zeros((train_waveforms.shape[0], 160))] # todo: fix net size or label size
+		val_waveforms = np.c_[val_waveforms, np.zeros((val_waveforms.shape[0], 160))]
+
+		train_waveforms = np.stack([train_waveforms] * 30, -1)
+		val_waveforms = np.stack([val_waveforms] * 30, -1)
+
+		model_cache = ModelCache(model_cache_dir)
+		checkpoint = ModelCheckpoint(model_cache.model_path(), verbose=1)
+
+		lr_decay = ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=5, min_lr=0, verbose=1)
+		early_stopping = EarlyStopping(monitor='val_loss', min_delta=0.01, patience=10, verbose=1)
+
+		self.__model.fit(
+			x=[train_enhanced_spectrograms], y=[train_waveforms],
+			validation_data=([val_enhanced_spectrograms], [val_waveforms]),
+			batch_size=1, epochs=1000,
+			callbacks=[checkpoint, lr_decay, early_stopping],
+			verbose=1
+		)
+
+	def predict_one_sample(self, enhanced_spectrograms):
+		params = self.__model.predict(enhanced_spectrograms, batch_size=1)
+		means = np.squeeze(params[:,:,:10])
+		sigmas = np.squeeze(np.abs(params[:,:,10:20]))
+		weights = np.squeeze(np.abs(params[:,:,20:]))
+
+		column_indices = sample_many_categorical_once(weights)
+		rows_indices = np.arange(means.shape[0])
+
+		means = means[rows_indices, column_indices]
+		sigmas = sigmas[rows_indices, column_indices]
+
+		return np.random.logistic(means, sigmas)
+
+	@staticmethod
+	def load(model_cache_dir):
+		model_cache = ModelCache(model_cache_dir)
+		model = load_model(model_cache.model_path(), custom_objects={'loss':loss, 'tf':tf})
+		vocoder = WavenetVocoder()
+		vocoder.__model = model
+
+		return vocoder
+
 
 def loss(y_true, y_pred):
-	means = y_pred[:,:10]
-	sigmas = y_pred[:,10:20]
-	weights = y_pred[:,20:]
+	y_true = tf.expand_dims(y_true[:,:,0])
+
+
+	means = y_pred[:,:,:10]
+	sigmas = y_pred[:,:,10:20]
+	weights = y_pred[:,:,20:]
+
+	sum_of_weights = tf.sum(weights, axis=2)
 
 	args = (y_true - means) / (2 * sigmas)
 	args = tf.stack([args, -args], axis=-1)
-	loglogistic = -tf.log(sigmas) -2 * tf.logsumexp(args, axis=-1)
-	logs = loglogistic + tf.log(weights)
+	loglogistic = -tf.log(tf.abs(sigmas)) - 2 * tf.logsumexp(args, axis=-1)
+	logs = loglogistic + tf.log(tf.abs(weights))
 	loglikelihood = tf.sum(tf.logsumexp(logs, axis=-1), axis=-1)
+	return -loglikelihood + (1 - sum_of_weights) ** 2
 
-	return loglikelihood
 
+def sample_many_categorical_once(weights):
+	norm_weights = weights / weights.sum(-1)[..., np.newaxis]
+	cdf = np.cumsum(norm_weights, -1)
+	n = weights.shape[-2] if weights.ndim > 1 else 1
+	uniform = np.random.uniform(size=n)
+	indices = (cdf <= uniform[:, np.newaxis]).argmin(-1)
 
+	return np.squeeze(indices)
 
 
 if __name__ == '__main__':
-	net = WavenetVocoder(80, 3)
-	# net.build_upsample_net((20, 80)).summary()
-	net.build((None, 80))
+	net = WavenetVocoder(80, 15, (80, 20))
 
 