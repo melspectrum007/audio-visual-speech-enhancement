6b086cd1cb5b6dd9a28aeea21cb9c4b2f089376f
Asaph <asaph.shamir@gmail.com>
2018-03-17 15:08:01 +0200

    remote debugging

--- check_something.py
+++ check_something.py
@@ -3,13 +3,15 @@ import numpy as np
 
 def split_and_concat(array, axis, split):
 	slc = [slice(None)] * array.ndim
-	slc[axis] = slice(0, -(array.shape[axis] % split))
+	mod = -(array.shape[axis] % split)
+	end = None if mod == 0 else mod
+	slc[axis] = slice(0, end)
 
 	return np.concatenate(np.split(array[slc], split, axis))
 
 
 a = np.zeros([3, 80, 248])
 
-b = split_and_concat(a, -1, 4)
+b = split_and_concat(a, -1, 6)
 
 print b.shape
