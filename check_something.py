cd9e6e8691031ec4680e6bb4b54f0b14efeb153e
asaph <asaph.shamir@gmail.com>
2018-04-10 13:12:35 +0300

    res net checkpoint

--- check_something.py
+++ check_something.py
@@ -1,17 +1,65 @@
 
 import numpy as np
+import matplotlib.pyplot as pl
+from scipy.signal import *
+from mediaio.audio_io import AudioSignal
+import librosa as lb
 
-def split_and_concat(array, axis, split):
-	slc = [slice(None)] * array.ndim
-	mod = -(array.shape[axis] % split)
-	end = None if mod == 0 else mod
-	slc[axis] = slice(0, end)
+s = AudioSignal.from_wav_file('/cs/grad/asaph/clean_sounds/pbai6a_s2.wav')
+g = AudioSignal.from_wav_file('/cs/grad/asaph/clean_sounds/geese.wav')
 
-	return np.concatenate(np.split(array[slc], split, axis))
+while g.get_number_of_samples() < s.get_number_of_samples():
+	g = AudioSignal.concat([g, g])
+
+g.truncate(s.get_number_of_samples())
 
 
-a = np.zeros([3, 80, 248])
+s = s.get_data().astype('f')
+g = g.get_data().astype('f')
 
-b = split_and_concat(a, -1, 6)
 
-print b.shape
+mag_s, phase_s = lb.magphase(lb.stft(s, 640, 160))
+mag_g, phase_g = lb.magphase(lb.stft(g, 640, 160))
+
+s_plus_g = lb.istft(mag_s * phase_g).astype(np.int16)
+g_plus_s = lb.istft(mag_g * phase_s).astype(np.int16)
+s = lb.istft(mag_s * phase_s).astype(np.int16)
+g = lb.istft(mag_g * phase_g).astype(np.int16)
+
+AudioSignal(s_plus_g, 16000).save_to_wav_file('/cs/grad/asaph/testing/s_g.wav')
+AudioSignal(g_plus_s, 16000).save_to_wav_file('/cs/grad/asaph/testing/g_s.wav')
+AudioSignal(s, 16000).save_to_wav_file('/cs/grad/asaph/testing/s_s.wav')
+AudioSignal(g, 16000).save_to_wav_file('/cs/grad/asaph/testing/g_g.wav')
+
+
+
+
+# raw_data = s.get_data().astype('f')
+#
+# pl.plot(raw_data)
+#
+# data = np.abs(raw_data)
+# # pl.figure()
+# # pl.plot(data)
+#
+# k = 25.
+#
+# med = medfilt(data, kernel_size=int(k))
+# mean = np.convolve(data, np.arange(k) / k, mode='same')
+#
+#
+# pl.figure()
+# pl.plot(mean)
+# # pl.figure()
+# # pl.plot(med)
+#
+# thresh = 10000
+#
+# line = (mean > thresh) * 10000
+# pl.figure(1)
+# pl.plot(line, 'r')
+#
+# pl.show()
+
+
+
--- source_enhancement_evaluator.py
+++ source_enhancement_evaluator.py
@@ -5,6 +5,7 @@ import tempfile
 import re
 from shutil import copy2
 import numpy as np
+import uuid
 
 from mediaio import ffmpeg
 
@@ -12,8 +13,9 @@ from mediaio import ffmpeg
 def pesq(pesq_bin_path, source_file_path, enhanced_file_path):
 	temp_dir = tempfile.gettempdir()
 
-	temp_source_path = os.path.join(temp_dir, 'source.wav')
-	temp_enhanced_path = os.path.join(temp_dir, 'enhanced.wav')
+	random_unique_prefix = str(uuid.uuid4())
+	temp_source_path = os.path.join(temp_dir, random_unique_prefix + 'source.wav')
+	temp_enhanced_path = os.path.join(temp_dir, random_unique_prefix + 'enhanced.wav')
 
 	copy2(source_file_path, temp_source_path)
 	copy2(enhanced_file_path, temp_enhanced_path)
@@ -23,7 +25,7 @@ def pesq(pesq_bin_path, source_file_path, enhanced_file_path):
 
 	try:
 		output = subprocess.check_output(
-			[pesq_bin_path, "+16000", temp_source_path, temp_enhanced_path]
+			[pesq_bin_path, '+16000', temp_source_path, temp_enhanced_path]
 		)
 	except Exception:
 		print 'exception'
@@ -31,7 +33,7 @@ def pesq(pesq_bin_path, source_file_path, enhanced_file_path):
 
 	# print output
 
-	match = re.search("\(Raw MOS, MOS-LQO\):\s+= (-?[0-9.]+?)\s+([0-9.]+?)$", output, re.MULTILINE)
+	match = re.search('\(Raw MOS, MOS-LQO\):\s+= (-?[0-9.]+?)\s+([0-9.]+?)$', output, re.MULTILINE)
 	mos = float(match.group(1))
 	moslqo = float(match.group(2))
 
@@ -45,26 +47,23 @@ def evaluate(enhancement_dir_path, pesq_bin_path):
 	enhanced_pesqs = []
 	mixture_pesqs = []
 
-	speaker_dir_names = os.listdir(enhancement_dir_path)
-	for speaker_dir_name in speaker_dir_names:
-		speaker_dir_path = os.path.join(enhancement_dir_path, speaker_dir_name)
-		sample_dir_names = sorted(os.listdir(speaker_dir_path))
-		for sample_dir_name in sample_dir_names:
-			print sample_dir_name
-			sample_dir_path = os.path.join(speaker_dir_path, sample_dir_name)
-			source_file_path = os.path.join(sample_dir_path, "source.wav")
-			enhanced_file_path = os.path.join(sample_dir_path, "enhanced.wav")
-			mixture_file_path = os.path.join(sample_dir_path, "mixture.wav")
+	sample_dir_names = os.listdir(enhancement_dir_path)
+	for sample_dir_name in sample_dir_names:
+		print sample_dir_name
+		sample_dir_path = os.path.join(enhancement_dir_path, sample_dir_name)
+		source_file_path = os.path.join(sample_dir_path, 'source.wav')
+		enhanced_file_path = os.path.join(sample_dir_path, 'enhanced.wav')
+		mixture_file_path = os.path.join(sample_dir_path, 'mixed.wav')
 
-			enhanced_mos, _ = pesq(pesq_bin_path, source_file_path, enhanced_file_path)
-			mixture_mos, _ = pesq(pesq_bin_path, source_file_path, mixture_file_path)
+		enhanced_mos, _ = pesq(pesq_bin_path, source_file_path, enhanced_file_path)
+		mixture_mos, _ = pesq(pesq_bin_path, source_file_path, mixture_file_path)
 
-			print 'mixture pesq: ', mixture_mos, 'enhanced pesq: ', enhanced_mos
-			if enhanced_mos is None or mixture_mos is None:
-				continue
-			else:
-				enhanced_pesqs.append(enhanced_mos)
-				mixture_pesqs.append(mixture_mos)
+		print 'mixture pesq: ', mixture_mos, 'enhanced pesq: ', enhanced_mos
+		if enhanced_mos is None or mixture_mos is None:
+			continue
+		else:
+			enhanced_pesqs.append(enhanced_mos)
+			mixture_pesqs.append(mixture_mos)
 
 	print 'mean enhanced pesq: ', np.mean(enhanced_pesqs), 'std enhanced pesq: ', np.std(enhanced_pesqs)
 	print 'mean mixture pesq: ', np.mean(mixture_pesqs), 'std mixture pesq: ', np.std(mixture_pesqs)
@@ -73,12 +72,12 @@ def evaluate(enhancement_dir_path, pesq_bin_path):
 
 def main():
 	parser = argparse.ArgumentParser()
-	parser.add_argument("enhancement_dir", type=str)
-	parser.add_argument("pesq_bin_path", type=str)
+	parser.add_argument('enhancement_dir', type=str)
+	parser.add_argument('pesq_bin_path', type=str)
 	args = parser.parse_args()
 
 	evaluate(args.enhancement_dir, args.pesq_bin_path)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
 	main()
